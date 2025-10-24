13e947f80ab97e34eff7878c4937a2dfaf2a2f7c
Srushti Amin <srushtiamin2@gmail.com>
2025-10-24 16:50:21 +0530

    mongo

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 
-const connectDb = async () =>{
-    try {
-        const connect = await mongoose.connect(process.env.CONNECTION_STRING);
-        console.log("Database Connected")
-    }catch{
-        console.log(err);
-        process.exit(1);
-    }
+const connectDb = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.CONNECTION_STRING);
+    console.log("Database Connected:", connect.connection.host, connect.connection.name);
+  } catch (err) {
+    console.error("Database Connection Failed");
+    console.error("Error Message:", err.message);
+    console.error("Full Error:", err);
+    process.exit(1);
+  }
 };
+
 module.exports = connectDb;
+ 
